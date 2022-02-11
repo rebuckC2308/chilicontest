0d847eedd5805ef75a1fe9709aa28137177282a2
JeremyDWarden <JeremyDWarden@gmail.com>
2022-02-10 23:25:35 -0600

    new images

--- chili/src/Helpers/createEntry.js
+++ chili/src/Helpers/createEntry.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 // eslint-disable-next-line import/no-unresolved
 import { BASEURL } from '@env';
 
@@ -11,11 +12,11 @@ export const createEntry = async ({
 }) => {
   const index = Math.floor(Math.random() * 10) % 5;
   const randomImages = [
-    'https://www.thewholesomedish.com/wp-content/uploads/2018/05/600-X-900.jpg',
+    'https://assets.epicurious.com/photos/5a9485cfd8f5fb6b3a2e1aed/master/w_1280,c_limit/shutterstock_313246001.jpg',
     'https://assets.epicurious.com/photos/54beca322a4b69f8647ef1a5/16:9/w_1280,c_limit/vegan-chili-6x4.jpg',
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuc-N8kYmfUeAJMHtMdKbK2Om2A-FHjfVgQA&usqp=CAU',
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReR-73O6Oz310sjqaObRoxNl3IlRMx9P_X9w&usqp=CAU',
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSG4BJnvODkVanuVdSC5x_6FNvBMob4RJpvVw&usqp=CAU'];
+    'https://assets.epicurious.com/photos/54ad58076529d92b2c044921/master/w_1280,c_limit/51201610_upstate-chili_1x1.jpg',
+    'https://assets.epicurious.com/photos/578d1fe51edb4f17303e3f0f/master/w_1280,c_limit/vegetarian-black-bean-chili.jpg',
+    'https://assets.epicurious.com/photos/54f89fd1e77a7de9330672b2/master/w_1280,c_limit/237096_beef-and-dark-beer-chili_6x4.jpg'];
 
   try {
     const response = await fetch(`${BASEURL}/createEntry`, {
