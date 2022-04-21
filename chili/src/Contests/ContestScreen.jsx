106e242248414717aecbc3de9fc5d8a4b8c9f845
Chris Rebuck <rebuckc2308@gmail.com>
2022-04-20 19:03:23 -0500

    can now see cards again

--- chili/src/Contests/ContestScreen.jsx
+++ chili/src/Contests/ContestScreen.jsx
@@ -10,10 +10,11 @@ import {
   TouchableWithoutFeedback,
   Keyboard,
   KeyboardAvoidingView,
-  FlatList
+  FlatList,
 } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { Card } from 'react-native-elements';
+import { useHeaderHeight } from '@react-navigation/elements';
 import { globalColors } from '../styles';
 import { styles as contestStyles } from './contestStyles';
 import { ErrorModal } from '../Modal/ErrorModal';
@@ -21,40 +22,36 @@ import { ModalContext } from '../Contexts/ModalContext';
 import { ContestContext } from '../Contexts/ContestContext';
 import { CreateEntryForm } from '../Components/CreateEntryForm';
 import { getAllEntries } from '../Helpers/getAllEntries';
-import { CameraContext } from '../Contexts/CameraContext';
-import { useHeaderHeight } from '@react-navigation/elements';
 
 // import { LoadingSpinner } from '../Components/LoadingSpinner';
 
 const { width, height } = Dimensions.get('window');
 
-function EntriesContent(entry) {
+function EntriesContent({ entry }) {
   const {
     name, imageurl, description,
   } = entry;
 
-  console.log({entry})
-
   return (
-      <Card containerStyle={
+    <Card containerStyle={
         {
           backgroundColor: globalColors.ORANGE,
           maxWidth: width,
           maxHeight: height * 0.8,
         }
       }
-      >
-        <Card.Title style={{ fontSize: 30, color: 'white' }}>{`${name}`}</Card.Title>
-        <Card.Divider />
-        <Image
-          style={{
-            minWidth: width * 0.8,
-            height: height * 0.45,
-          }}
-          source={{uri: imageurl}}
-        />
-        <Text>{`${description}`}</Text>
-      </Card>
+    >
+      <Card.Title style={{ fontSize: 30, color: 'white' }}>{`${name}`}</Card.Title>
+      <Card.Divider />
+      <Image
+        style={{
+          minWidth: width * 0.8,
+          height: height * 0.45,
+        }}
+        source={{ uri: imageurl }}
+      />
+      <Text>{`${description}`}</Text>
+    </Card>
   );
 }
 
@@ -67,22 +64,20 @@ function NoEntriesComponent() {
 }
 
 function EntriesCardView({ entries }) {
-
   return (
     <ScrollView
       horizontal
       showsHorizontalScrollIndicator={false}
       bounces
     >
-      {entries &&
+      {entries
+        && (
         <FlatList
           horizontal
           data={entries}
-          renderItem={(entry) => {
-            return <EntriesContent entry={entry} key={entry.id} />
-          }}
+          renderItem={({ item }) => <EntriesContent entry={item} key={item.id} />}
         />
-      }
+        )}
     </ScrollView>
   );
 }
@@ -131,8 +126,7 @@ function canelButton() {
   } = useContext(ContestContext);
 
   const buttonSize = showCreateEntryForm ? 15 : 20;
-  const buttonStyle = contestStyles.cancelButton
-
+  const buttonStyle = contestStyles.cancelButton;
 
   return (
     <TouchableOpacity
@@ -143,16 +137,16 @@ function canelButton() {
       <View
         style={buttonStyle}
       >
-          {!showCreateEntryForm && (
+        {!showCreateEntryForm && (
           <View style={{ justifyContent: 'center' }}>
             <Ionicons name="md-add" size={30} color="white" />
           </View>
-          )}
-          <View style={{ justifyContent: 'center' }}>
-            <Text style={{ fontWeight: 'bold', color: 'white', fontSize: buttonSize }}>
-              Cancel
-            </Text>
-          </View>
+        )}
+        <View style={{ justifyContent: 'center' }}>
+          <Text style={{ fontWeight: 'bold', color: 'white', fontSize: buttonSize }}>
+            Cancel
+          </Text>
+        </View>
       </View>
     </TouchableOpacity>
   );
@@ -179,37 +173,39 @@ export function ContestScreen({ navigation }) {
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
       <KeyboardAvoidingView
-        behavior={Platform.OS === "ios" ? "padding" : "height"}
-        keyboardVerticalOffset = {headerHeight}
+        // eslint-disable-next-line no-undef
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        keyboardVerticalOffset={headerHeight}
         style={contestStyles.container}
       >
-          <ErrorModal
-            setShouldDisplayErrorModal={setShouldDisplayErrorModal}
-            shouldDisplayErrorModal={shouldDisplayErrorModal}
-            errorModalText={errorModalText}
-          />
-          {!showCreateEntryForm && (
-            <View style={contestStyles.mainContent}>
-              {entries.length
-                ? <EntriesCardView entries={entries} />
-                : <NoEntriesComponent />}
-            </View>
-          )}
-          {!showCreateEntryForm &&
+        <ErrorModal
+          setShouldDisplayErrorModal={setShouldDisplayErrorModal}
+          shouldDisplayErrorModal={shouldDisplayErrorModal}
+          errorModalText={errorModalText}
+        />
+        {!showCreateEntryForm && (
+        <View style={contestStyles.mainContent}>
+          {entries.length
+            ? <EntriesCardView entries={entries} />
+            : <NoEntriesComponent />}
+        </View>
+        )}
+        {!showCreateEntryForm
+            && (
             <View style={contestStyles.buttonContainer}>
               <AddEntryButton />
             </View>
-          }
-          {showCreateEntryForm && (
-            <View style={contestStyles.mainContent}>
-              <CreateEntryForm
-                setShouldFetchEntries={setShouldFetchEntries}
-                setShowCreateEntryForm={setShowCreateEntryForm}
-                navigation={navigation}
-                cancelButton={canelButton}
-              />
-            </View>
-          )}
+            )}
+        {showCreateEntryForm && (
+        <View style={contestStyles.mainContent}>
+          <CreateEntryForm
+            setShouldFetchEntries={setShouldFetchEntries}
+            setShowCreateEntryForm={setShowCreateEntryForm}
+            navigation={navigation}
+            cancelButton={canelButton}
+          />
+        </View>
+        )}
       </KeyboardAvoidingView>
     </TouchableWithoutFeedback>
   );
